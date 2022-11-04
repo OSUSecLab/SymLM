a178c7a33ad67634a558b75e272ccaa2fa62f4c1
xin <xin.jin0011@gmail.com>
2022-11-03 22:21:45 -0400

    add params

--- fairseq/modules/transformer_sentence_encoder_mf_nau.py
+++ fairseq/modules/transformer_sentence_encoder_mf_nau.py
@@ -16,7 +16,10 @@ from fairseq.modules import (
 )
 from fairseq.modules.nau import ReRegualizedLinearNACLayer
 from fairseq.modules.quant_noise import quant_noise as apply_quant_noise_
-from command import params
+try:
+    from command import params
+except ImportError:
+    from . import params
 
 
 def init_bert_params(module):
